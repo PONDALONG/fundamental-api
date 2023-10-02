7766e236ceba2ff214c9416f2ae08978770a8f58
PONDALONG <alongkorn.mo1999@gmail.com>
2023-10-02 22:44:33 +0700

    Update file-resource.controller.ts

--- src/file-resource/file-resource.controller.ts
+++ src/file-resource/file-resource.controller.ts
@@ -1,8 +1,27 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
 import { FileResourceService } from "./file-resource.service";
-
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { createWriteStream } from "fs";
 @Controller("file-resource")
 export class FileResourceController {
   constructor(private readonly fileResourceService: FileResourceService) {
   }
+
+  @Post("upload")
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: "files" }
+  ]))
+  uploadFile(@UploadedFiles() files?: Array<Express.Multer.File>) {
+    const listFile: Express.Multer.File[] = files["files"];
+    for (const listFileElement of listFile) {
+      console.log(listFileElement.originalname);
+      const destinationPath = `./uploads/${listFileElement.originalname}`;
+
+      // Use fs.promises to save the file
+       createWriteStream(destinationPath).write(listFileElement.buffer);
+
+    }
+    return true
+  }
+
 }
